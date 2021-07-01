afdbe7b3aaee3527ca84a6d61733f7bd27feca97
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-07-01 19:11:30 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Navigation from './components/navigation/Navigation';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
-import Home from './app/Home';
+import Home from './app/home/Home';
+import Input from './app/input/Input';
+import Result from './app/input/Result';
+
 function App() {
   return (
     <>
@@ -15,6 +18,12 @@ function App() {
           <Switch>
             <Route path={'/home'}>
               <Home />
+            </Route>
+            <Route path={'/input'}>
+              <Input />
+            </Route>
+            <Route path={'/result'}>
+              <Result />
             </Route>
             <Route path={'/'}>
               <Home />
